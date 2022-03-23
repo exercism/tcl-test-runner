2f84cc1168605dd636f6edc4080b8c5f22ef3e25
Glenn Jackman <jackman@pythian.com>
2022-03-23 18:48:38 -0400

    rewrite tests using tcltest

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -11,32 +11,4 @@
 # Example:
 # ./bin/run-tests.sh
 
-exit_code=0
-
-export RUN_ALL=yes
-
-# Iterate over all test directories
-for test_dir in tests/*; do
-    test_dir_name="$(basename $test_dir)"
-    test_dir_path="$(realpath $test_dir)"
-    results_file="results.json"
-    results_file_path="${test_dir}/results.json"
-    expected_results_file="expected_results.json"
-    expected_results_file_path="${test_dir}/expected_results.json"    
-
-    if [ -f "${expected_results_file_path}" ]; then
-        echo "===="
-        bin/run.tcl "${test_dir_name}" "${test_dir}" "${test_dir}"
-        echo "===="
-
-        echo "${test_dir_name}: comparing ${results_file} to ${expected_results_file}"
-        if diff "${results_file_path}" "${expected_results_file_path}"; then
-            # exit status zero: no diff
-            echo OK
-        else
-            exit_code=1
-        fi
-    fi
-done
-
-exit ${exit_code}
+tclsh tests/test_all.test
